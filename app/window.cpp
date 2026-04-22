a344cd58f632fcd7b52d11c126251734e2556aaf
poneding <poneding@gmail.com>
2026-04-22 09:05:33 +0800

    wire input callbacks into the window shell
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- app/window.cpp
+++ app/window.cpp
@@ -8,6 +8,61 @@ namespace {
 
 constexpr wchar_t kWindowClassName[] = L"SupraViewWindowClass";
 
+bool UpdateWindowLongPtr(HWND windowHandle, int index, LONG_PTR value) {
+  SetLastError(ERROR_SUCCESS);
+  const LONG_PTR result = SetWindowLongPtrW(windowHandle, index, value);
+  return result != 0 || GetLastError() == ERROR_SUCCESS;
+}
+
+supra::input::InputSurfacePoint MakeInputSurfacePointFromWindowClient(LPARAM lParam) {
+  supra::input::InputSurfacePoint point;
+  point.x = GET_X_LPARAM(lParam);
+  point.y = GET_Y_LPARAM(lParam);
+  return point;
+}
+
+supra::input::MouseButton ResolveMouseButton(UINT message, WPARAM wParam) {
+  switch (message) {
+    case WM_LBUTTONDOWN:
+    case WM_LBUTTONUP:
+      return supra::input::MouseButton::Left;
+    case WM_RBUTTONDOWN:
+    case WM_RBUTTONUP:
+      return supra::input::MouseButton::Right;
+    case WM_MBUTTONDOWN:
+    case WM_MBUTTONUP:
+      return supra::input::MouseButton::Middle;
+    case WM_XBUTTONDOWN:
+    case WM_XBUTTONUP:
+      return GET_XBUTTON_WPARAM(wParam) == XBUTTON1 ? supra::input::MouseButton::X1
+                                                    : supra::input::MouseButton::X2;
+    default:
+      return supra::input::MouseButton::Left;
+  }
+}
+
+supra::input::ButtonTransition ResolveButtonTransition(UINT message) {
+  switch (message) {
+    case WM_LBUTTONDOWN:
+    case WM_RBUTTONDOWN:
+    case WM_MBUTTONDOWN:
+    case WM_XBUTTONDOWN:
+      return supra::input::ButtonTransition::Pressed;
+    default:
+      return supra::input::ButtonTransition::Released;
+  }
+}
+
+supra::input::KeyTransition ResolveKeyTransition(UINT message) {
+  switch (message) {
+    case WM_KEYDOWN:
+    case WM_SYSKEYDOWN:
+      return supra::input::KeyTransition::Pressed;
+    default:
+      return supra::input::KeyTransition::Released;
+  }
+}
+
 }  // namespace
 
 bool Window::Create(std::wstring_view title, int clientWidth, int clientHeight) {
@@ -51,6 +106,12 @@ void Window::Destroy() {
     DestroyWindow(windowHandle_);
     windowHandle_ = nullptr;
   }
+
+  hasInputFocus_ = false;
+  borderlessFullscreen_ = false;
+  windowedStyle_ = 0;
+  windowedExStyle_ = 0;
+  windowedPlacement_ = WINDOWPLACEMENT{sizeof(WINDOWPLACEMENT)};
 }
 
 bool Window::PumpMessages() {
@@ -64,6 +125,141 @@ bool Window::PumpMessages() {
     DispatchMessageW(&message);
   }
 
+  return true;
+}
+
+bool Window::SetBorderlessFullscreen(bool enabled) {
+  if (windowHandle_ == nullptr) {
+    return false;
+  }
+
+  if (enabled == borderlessFullscreen_) {
+    return true;
+  }
+
+  return enabled ? EnterBorderlessFullscreen() : ExitBorderlessFullscreen();
+}
+
+bool Window::ToggleBorderlessFullscreen() {
+  return SetBorderlessFullscreen(!borderlessFullscreen_);
+}
+
+Window* Window::FromHandle(HWND windowHandle) {
+  if (windowHandle == nullptr) {
+    return nullptr;
+  }
+
+  return reinterpret_cast<Window*>(GetWindowLongPtrW(windowHandle, GWLP_USERDATA));
+}
+
+bool Window::EnterBorderlessFullscreen() {
+  MONITORINFO monitorInfo{};
+  monitorInfo.cbSize = sizeof(monitorInfo);
+  const HMONITOR monitorHandle = MonitorFromWindow(windowHandle_, MONITOR_DEFAULTTONEAREST);
+  if (monitorHandle == nullptr || !GetMonitorInfoW(monitorHandle, &monitorInfo)) {
+    return false;
+  }
+
+  windowedPlacement_.length = sizeof(windowedPlacement_);
+  if (!GetWindowPlacement(windowHandle_, &windowedPlacement_)) {
+    return false;
+  }
+
+  windowedStyle_ = GetWindowLongPtrW(windowHandle_, GWL_STYLE);
+  windowedExStyle_ = GetWindowLongPtrW(windowHandle_, GWL_EXSTYLE);
+
+  RECT windowedRect{};
+  const bool hasWindowedRect = GetWindowRect(windowHandle_, &windowedRect) != FALSE;
+
+  const auto restoreWindowedShellState = [&]() {
+    bool restored = UpdateWindowLongPtr(windowHandle_, GWL_STYLE, windowedStyle_);
+    restored = UpdateWindowLongPtr(windowHandle_, GWL_EXSTYLE, windowedExStyle_) && restored;
+
+    restored = SetWindowPlacement(windowHandle_, &windowedPlacement_) && restored;
+
+    if (hasWindowedRect) {
+      restored = SetWindowPos(windowHandle_, nullptr, windowedRect.left, windowedRect.top,
+                              windowedRect.right - windowedRect.left,
+                              windowedRect.bottom - windowedRect.top,
+                              SWP_FRAMECHANGED | SWP_NOOWNERZORDER | SWP_NOZORDER) &&
+                 restored;
+    }
+
+    return restored;
+  };
+
+  const LONG_PTR fullscreenStyle = windowedStyle_ & ~static_cast<LONG_PTR>(WS_OVERLAPPEDWINDOW);
+  const LONG_PTR fullscreenExStyle = windowedExStyle_ & ~static_cast<LONG_PTR>(WS_EX_OVERLAPPEDWINDOW);
+  const bool updatedStyle = UpdateWindowLongPtr(windowHandle_, GWL_STYLE, fullscreenStyle);
+  const bool updatedExStyle = UpdateWindowLongPtr(windowHandle_, GWL_EXSTYLE, fullscreenExStyle);
+  if (!updatedStyle || !updatedExStyle) {
+    restoreWindowedShellState();
+    return false;
+  }
+
+  const RECT monitorRect = monitorInfo.rcMonitor;
+  if (!SetWindowPos(windowHandle_, HWND_TOP, monitorRect.left, monitorRect.top,
+                    monitorRect.right - monitorRect.left, monitorRect.bottom - monitorRect.top,
+                    SWP_FRAMECHANGED | SWP_NOOWNERZORDER)) {
+    restoreWindowedShellState();
+    return false;
+  }
+
+  borderlessFullscreen_ = true;
+  return true;
+}
+
+bool Window::ExitBorderlessFullscreen() {
+  const LONG_PTR fullscreenStyle = GetWindowLongPtrW(windowHandle_, GWL_STYLE);
+  const LONG_PTR fullscreenExStyle = GetWindowLongPtrW(windowHandle_, GWL_EXSTYLE);
+
+  RECT fullscreenRect{};
+  const bool hasFullscreenRect = GetWindowRect(windowHandle_, &fullscreenRect) != FALSE;
+
+  WINDOWPLACEMENT fullscreenPlacement{sizeof(WINDOWPLACEMENT)};
+  const bool hasFullscreenPlacement =
+      GetWindowPlacement(windowHandle_, &fullscreenPlacement) != FALSE;
+
+  const auto restoreFullscreenShellState = [&]() {
+    bool restored = UpdateWindowLongPtr(windowHandle_, GWL_STYLE, fullscreenStyle);
+    restored = UpdateWindowLongPtr(windowHandle_, GWL_EXSTYLE, fullscreenExStyle) && restored;
+
+    if (hasFullscreenPlacement) {
+      restored = SetWindowPlacement(windowHandle_, &fullscreenPlacement) && restored;
+    }
+
+    if (hasFullscreenRect) {
+      restored = SetWindowPos(windowHandle_, nullptr, fullscreenRect.left, fullscreenRect.top,
+                              fullscreenRect.right - fullscreenRect.left,
+                              fullscreenRect.bottom - fullscreenRect.top,
+                              SWP_FRAMECHANGED | SWP_NOOWNERZORDER | SWP_NOZORDER) &&
+                 restored;
+    }
+
+    return restored;
+  };
+
+  const bool restoredStyle = UpdateWindowLongPtr(windowHandle_, GWL_STYLE, windowedStyle_);
+  const bool restoredExStyle = UpdateWindowLongPtr(windowHandle_, GWL_EXSTYLE, windowedExStyle_);
+  if (!restoredStyle || !restoredExStyle) {
+    restoreFullscreenShellState();
+    return false;
+  }
+
+  windowedPlacement_.length = sizeof(windowedPlacement_);
+  if (!SetWindowPlacement(windowHandle_, &windowedPlacement_)) {
+    restoreFullscreenShellState();
+    return false;
+  }
+
+  if (!SetWindowPos(windowHandle_, nullptr, 0, 0, 0, 0,
+                    SWP_FRAMECHANGED | SWP_NOMOVE | SWP_NOSIZE | SWP_NOOWNERZORDER |
+                        SWP_NOZORDER)) {
+    restoreFullscreenShellState();
+    return false;
+  }
+
+  borderlessFullscreen_ = false;
   return true;
 }
 
@@ -87,6 +283,12 @@ LRESULT CALLBACK Window::StaticWindowProc(HWND hwnd, UINT message, WPARAM wParam
 
 LRESULT Window::WindowProc(UINT message, WPARAM wParam, LPARAM lParam) {
   switch (message) {
+    case WM_SETFOCUS:
+      hasInputFocus_ = true;
+      return 0;
+    case WM_KILLFOCUS:
+      hasInputFocus_ = false;
+      return 0;
     case WM_SIZE: {
       clientWidth_ = LOWORD(lParam);
       clientHeight_ = HIWORD(lParam);
@@ -95,7 +297,49 @@ LRESULT Window::WindowProc(UINT message, WPARAM wParam, LPARAM lParam) {
       }
       return 0;
     }
+    case WM_MOUSEMOVE: {
+      if (hasInputFocus_ && mouseMoveCallback_ != nullptr) {
+        mouseMoveCallback_(
+            supra::input::MouseMoveEvent{MakeInputSurfacePointFromWindowClient(lParam)});
+      }
+      return 0;
+    }
+    case WM_LBUTTONDOWN:
+    case WM_LBUTTONUP:
+    case WM_RBUTTONDOWN:
+    case WM_RBUTTONUP:
+    case WM_MBUTTONDOWN:
+    case WM_MBUTTONUP:
+    case WM_XBUTTONDOWN:
+    case WM_XBUTTONUP: {
+      if (hasInputFocus_ && mouseButtonCallback_ != nullptr) {
+        supra::input::MouseButtonEvent mouseButtonEvent;
+        mouseButtonEvent.button = ResolveMouseButton(message, wParam);
+        mouseButtonEvent.transition = ResolveButtonTransition(message);
+        mouseButtonEvent.inputSurfacePosition = MakeInputSurfacePointFromWindowClient(lParam);
+        mouseButtonCallback_(mouseButtonEvent);
+      }
+
+      return message == WM_XBUTTONDOWN || message == WM_XBUTTONUP ? TRUE : 0;
+    }
+    case WM_KEYDOWN:
+    case WM_KEYUP:
+    case WM_SYSKEYDOWN:
+    case WM_SYSKEYUP: {
+      if (hasInputFocus_ && keyboardCallback_ != nullptr) {
+        supra::input::KeyboardEvent keyboardEvent;
+        keyboardEvent.virtualKey = static_cast<std::uint32_t>(wParam);
+        keyboardEvent.transition = ResolveKeyTransition(message);
+        keyboardEvent.wasPreviouslyDown = (lParam & (1UL << 30)) != 0;
+        keyboardEvent.isExtendedKey = (lParam & (1UL << 24)) != 0;
+        keyboardEvent.repeatCount = static_cast<std::uint16_t>(lParam & 0xFFFFUL);
+        keyboardCallback_(keyboardEvent);
+      }
+
+      break;
+    }
     case WM_DESTROY:
+      hasInputFocus_ = false;
       PostQuitMessage(0);
       return 0;
     default:
--- app/window.h
+++ app/window.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "../core/input/input_state.h"
+
 #include <cstdint>
 #include <functional>
 #include <string_view>
@@ -9,41 +11,71 @@
 
 #include <windows.h>
 
-namespace supra::app {
+namespace supra {
+namespace app {
 
 class Window {
  public:
   using ResizeCallback = std::function<void(std::uint32_t, std::uint32_t)>;
+  using MouseMoveCallback = std::function<void(const supra::input::MouseMoveEvent&)>;
+  using MouseButtonCallback = std::function<void(const supra::input::MouseButtonEvent&)>;
+  using KeyboardCallback = std::function<void(const supra::input::KeyboardEvent&)>;
 
   bool Create(std::wstring_view title, int clientWidth, int clientHeight);
   void Destroy();
   bool PumpMessages();
+  bool SetBorderlessFullscreen(bool enabled);
+  bool ToggleBorderlessFullscreen();
 
   void SetResizeCallback(ResizeCallback callback) { resizeCallback_ = std::move(callback); }
+  void SetMouseMoveCallback(MouseMoveCallback callback) { mouseMoveCallback_ = std::move(callback); }
+  void SetMouseButtonCallback(MouseButtonCallback callback) {
+    mouseButtonCallback_ = std::move(callback);
+  }
+  void SetKeyboardCallback(KeyboardCallback callback) { keyboardCallback_ = std::move(callback); }
+
+  static Window* FromHandle(HWND windowHandle);
 
   [[nodiscard]] HWND Handle() const noexcept { return windowHandle_; }
   [[nodiscard]] UINT ClientWidth() const noexcept { return clientWidth_; }
   [[nodiscard]] UINT ClientHeight() const noexcept { return clientHeight_; }
+  [[nodiscard]] bool IsBorderlessFullscreen() const noexcept { return borderlessFullscreen_; }
 
  private:
+  bool EnterBorderlessFullscreen();
+  bool ExitBorderlessFullscreen();
+
   static LRESULT CALLBACK StaticWindowProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
   LRESULT WindowProc(UINT message, WPARAM wParam, LPARAM lParam);
 
   HWND windowHandle_ = nullptr;
   UINT clientWidth_ = 0;
   UINT clientHeight_ = 0;
+  bool hasInputFocus_ = false;
+  bool borderlessFullscreen_ = false;
+  LONG_PTR windowedStyle_ = 0;
+  LONG_PTR windowedExStyle_ = 0;
+  WINDOWPLACEMENT windowedPlacement_{sizeof(WINDOWPLACEMENT)};
   ResizeCallback resizeCallback_;
+  MouseMoveCallback mouseMoveCallback_;
+  MouseButtonCallback mouseButtonCallback_;
+  KeyboardCallback keyboardCallback_;
 };
 
-}  // namespace supra::app
+}  // namespace app
+}  // namespace supra
 
 #else
 
-namespace supra::app {
+namespace supra {
+namespace app {
 
 class Window {
  public:
   using ResizeCallback = std::function<void(std::uint32_t, std::uint32_t)>;
+  using MouseMoveCallback = std::function<void(const supra::input::MouseMoveEvent&)>;
+  using MouseButtonCallback = std::function<void(const supra::input::MouseButtonEvent&)>;
+  using KeyboardCallback = std::function<void(const supra::input::KeyboardEvent&)>;
 
   bool Create(std::wstring_view, int clientWidth, int clientHeight) {
     clientWidth_ = static_cast<std::uint32_t>(clientWidth);
@@ -52,17 +84,30 @@ class Window {
   }
   void Destroy() {}
   bool PumpMessages() { return false; }
+  bool SetBorderlessFullscreen(bool) { return false; }
+  bool ToggleBorderlessFullscreen() { return false; }
   void SetResizeCallback(ResizeCallback callback) { resizeCallback_ = std::move(callback); }
+  void SetMouseMoveCallback(MouseMoveCallback callback) { mouseMoveCallback_ = std::move(callback); }
+  void SetMouseButtonCallback(MouseButtonCallback callback) {
+    mouseButtonCallback_ = std::move(callback);
+  }
+  void SetKeyboardCallback(KeyboardCallback callback) { keyboardCallback_ = std::move(callback); }
+  static Window* FromHandle(void*) { return nullptr; }
   [[nodiscard]] void* Handle() const noexcept { return nullptr; }
   [[nodiscard]] std::uint32_t ClientWidth() const noexcept { return clientWidth_; }
   [[nodiscard]] std::uint32_t ClientHeight() const noexcept { return clientHeight_; }
+  [[nodiscard]] bool IsBorderlessFullscreen() const noexcept { return false; }
 
  private:
   std::uint32_t clientWidth_ = 0;
   std::uint32_t clientHeight_ = 0;
   ResizeCallback resizeCallback_;
+  MouseMoveCallback mouseMoveCallback_;
+  MouseButtonCallback mouseButtonCallback_;
+  KeyboardCallback keyboardCallback_;
 };
 
-}  // namespace supra::app
+}  // namespace app
+}  // namespace supra
 
 #endif
